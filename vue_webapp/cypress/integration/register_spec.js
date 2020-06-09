5bd278a7ba4a57fd2cfefe0b4a3aa1820e6cf793
Jop <jop.huijbers2001@gmail.com>
2020-06-09 16:45:31 +0200

    changed test register

--- vue_webapp/cypress/integration/register_spec.js
+++ vue_webapp/cypress/integration/register_spec.js
@@ -3,7 +3,7 @@ describe('Register test', () => {
       cy.visit('http://localhost:8080/register');
     });
     it('Can register', () => {
-      cy.get('[data-type="email"]').clear().type('test@test.test2').should('have.value', 'test@test.test2');
+      cy.get('[data-type="email"]').clear().type('test@test.test3').should('have.value', 'test@test.test3');
       cy.get('[data-type="password"]').clear().type('Testtest').should('have.value', 'Testtest');
       cy.get('[data-type="re-password"]').clear().type('Testtest').should('have.value', 'Testtest');
   
