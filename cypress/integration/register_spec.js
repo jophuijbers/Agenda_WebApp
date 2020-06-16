f6f0018e94459728c357086bf642ce03c4bb8ada
Jop <jop.huijbers2001@gmail.com>
2020-06-16 14:48:05 +0200

    try

--- cypress/integration/register_spec.js
+++ cypress/integration/register_spec.js
@@ -3,7 +3,7 @@ describe('Register test', () => {
       cy.visit('/register');
     });
     it('Can register', () => {
-      cy.get('[data-type="email"]').clear().type('test@test.test3').should('have.value', 'test@test.test3');
+      cy.get('[data-type="email"]').clear().type('test@test.test4').should('have.value', 'test@test.test4');
       cy.get('[data-type="password"]').clear().type('Testtest').should('have.value', 'Testtest');
       cy.get('[data-type="re-password"]').clear().type('Testtest').should('have.value', 'Testtest');
   
