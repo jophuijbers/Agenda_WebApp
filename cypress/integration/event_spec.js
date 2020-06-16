efc2fa812dfe64e191d69f343fd0a53eccff9956
Jop <jop.huijbers2001@gmail.com>
2020-06-16 15:50:23 +0200

    test

--- cypress/integration/event_spec.js
+++ cypress/integration/event_spec.js
@@ -1,6 +1,6 @@
 describe('Add single day event test', () => {
     before(() => {
-      cy.request('localhost:8080/addevent');
+      cy.request('https://i430817.hera.fhict.nl/addevent');
     });
     it('Can create event', () => {
         
@@ -18,7 +18,7 @@ describe('Add single day event test', () => {
 
   describe('Add multiple day event test', () => {
     before(() => {
-      cy.request('localhost:8080/addevent');
+      cy.request('https://i430817.hera.fhict.nl/addevent');
     });
     it('Can create event', () => {
         
